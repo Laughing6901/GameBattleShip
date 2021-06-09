6785b2caa5cf3ce72d5c57ea0a8b9d9f702c708f
Anh Tai <nducanhtai@gmail.com>
2021-06-09 22:58:54 +0700

    Game class done

---
+++ public/src/Game.js
@@ -0,0 +1,43 @@
+import Player from "./Player.js";
+import Computer from "./Computer.js";
+
+export default class Game {
+  // Player
+  #player;
+  #computer;
+
+  constructor() {
+    // Create Player -> pass turn as param
+    this.#player = new Player(true);
+    this.#computer = new Computer(false);
+  }
+
+  swapTurn() {}
+
+  preload(scene) {
+    this.#player.preload(scene);
+    this.#computer.preload(scene);
+  }
+
+  create(scene) {
+    switch (scene) {
+      case "SetupScene":
+        this.#player.create(scene);
+        break;
+      case "ComputerScene":
+        this.#computer.create(scene, () => this.swapTurn());
+        break;
+      case "PlayerScene":
+        this.#player.create(scene, () => this.swapTurn());
+        break;
+    }
+  }
+
+  getPlayer() {
+    return this.#player;
+  }
+
+  getComputer() {
+    return this.#computer;
+  }
+}
