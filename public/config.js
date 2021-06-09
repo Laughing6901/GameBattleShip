4b2fb09d0e28e0a1dab2f38162bf1a88d93b7c5f
Anh Viet <anhviet5121pc@gmail.com>
2021-06-09 22:21:44 +0700

    Add config file

---
+++ public/config.js
@@ -0,0 +1,33 @@
+// Constant config for battleship game
+
+export const config = {
+  // Game
+  phaser: {
+    width: 800,
+    height: 600,
+  },
+  // Board size
+  board: {
+    width: 10,
+    height: 10,
+  },
+  // Square setup
+  square: {
+    width: 50,
+    height: 50,
+  },
+  //ship panel
+  ship: {
+    width: 275,
+    height: 70,
+  },
+  // These 2 objects belong to each other. If change in one, take a look at the
+  // other
+  shipsArr: [
+    { ship2: 2 },
+    { ship3: 3 },
+    { ship32: 3 },
+    { ship4: 4 },
+    { ship5: 5 },
+  ],
+};
